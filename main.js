15bc10de14e2998751a217341305f5c8177926a6
Elza Gazizova <elza396@yandex.ru>
2023-05-20 18:18:52 +0500

    chore: add comments

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const labFunction = () =>  {
-    const spanA = -1;
+    const spanA = -1; // концы отрезка a и b
     const spanB = 0;
     const coeff1 = 3; // вариант 3
     const coeff2 = 6;
     const coeff3 = -3;
     const coeff4 = 1;
-    const eps = 0.000001;
+    const eps = 0.000001; // точность
+
+    // вывод уравнение на HTML
+    const equationDiv = document.querySelector(".equation");
+    equationDiv.innerHTML = "Вариант 3: x<sup>3</sup>-3x<sup>2</sup>+6x+3=0"
 
     const functionBy =  (x) => {
         return coeff4*Math.pow(x,3) + coeff3*Math.pow(x,2) + coeff2*x + coeff1;
@@ -18,7 +22,7 @@ const labFunction = () =>  {
         return 6*coeff4*x + 2*coeff3;
     }
 
-    const getSpans = () => {
+    const getSpans = () => { // возвращает массив концов отрезка (первый - неподвижный)
         if (functionBy(spanA) * getSecondDerivative(spanA) > 0) {
             return [spanA, spanB] // первое значение неподвижно
         } else {
@@ -26,12 +30,13 @@ const labFunction = () =>  {
         }
     }
 
-    let counter = 0
+    let counter = 0 // счетчик
+
+    // HalfDivisionMethod
     let a = spanA;
     let b = spanB;
     let c;
 
-    // HalfDivisionMethod
     while (Math.abs(b - a) >= eps) { // Math.abs - модуль
         counter++;
         c = (a + b) / 2;
@@ -44,33 +49,36 @@ const labFunction = () =>  {
         }
     }
 
-    const halfDivisionTds = document.querySelectorAll(".halfDivision > *");
+    const halfDivisionTds = document.querySelectorAll(".halfDivision > *"); // заполнение таблицы в HTML
     halfDivisionTds[1].innerHTML = `${spanA}, ${spanB}, ${(spanA + spanB) /2}`;
     halfDivisionTds[2].innerHTML = `${c}`;
     halfDivisionTds[3].innerHTML = `${counter}`;
 
     //Chord Method
     let xNext;
-    let [xFixed, xCurr] = getSpans();
-    counter = 0;
+    let [xFixed, xCurr] = getSpans(); // неподвижный и подвижный (начальное приближение) конец отрезка (Деструктуризация)
+    counter = 0; // обнуляем счетчик
 
+    // заполнение таблицы в HTML
     const chordMethodTds = document.querySelectorAll(".chordMethod > *");
     chordMethodTds[1].innerHTML = `${spanA}, ${spanB}, ${xCurr}`;
 
-    while (Math.abs(xFixed - xCurr) >= eps) {
+    while (Math.abs(xFixed - xCurr) >= eps) { // сам метод
         xNext = xFixed - functionBy(xFixed) * (xCurr - xFixed) / (functionBy(xCurr) - functionBy(xFixed));
         xCurr = xFixed;
         xFixed = xNext;
         counter++;
     }
 
+    // заполнение таблицы в HTML
     chordMethodTds[2].innerHTML = `${xNext}`;
     chordMethodTds[3].innerHTML = `${counter}`;
 
     // Tangent Method
-    let [firstX, lastX] = getSpans();
+    let [firstX, lastX] = getSpans(); // за начальное приближение берем первое
     counter = 0;
 
+    // заполнение таблицы в HTML
     const tangentMethodTds = document.querySelectorAll(".tangentMethod > *");
     tangentMethodTds[1].innerHTML = `${spanA}, ${spanB}, ${firstX}`;
 
@@ -81,6 +89,7 @@ const labFunction = () =>  {
         counter++;
     }
 
+    // заполнение таблицы в HTML
     tangentMethodTds[2].innerHTML = `${xNext}`;
     tangentMethodTds[3].innerHTML = `${counter}`;
 
@@ -89,6 +98,7 @@ const labFunction = () =>  {
     let aNext, bNext;
     counter = 0;
 
+    // заполнение таблицы в HTML
     const combinedMethodTds = document.querySelectorAll(".combinedMethod > *");
     combinedMethodTds[1].innerHTML = `${spanA}, ${spanB}, ${a}`;
 
@@ -100,7 +110,8 @@ const labFunction = () =>  {
         counter++;
     }
 
-    combinedMethodTds[2].innerHTML = `${aNext}, ${bNext}`;
+    // заполнение таблицы в HTML
+    combinedMethodTds[2].innerHTML = `${aNext}`;
     combinedMethodTds[3].innerHTML = `${counter}`;
 
     // Iteration Method
@@ -111,6 +122,7 @@ const labFunction = () =>  {
     const qMax = Math.max(getDerivative(a), getDerivative(b));
     const k = Math.ceil(qMax/2); // округление до целого числа в большую сторону
 
+    // заполнение таблицы в HTML
     const iterationMethodTds = document.querySelectorAll(".iterationMethod > *");
     iterationMethodTds[1].innerHTML = `${spanA}, ${spanB}, ${a}`; // начальное выбираем любое значение из интервала
 
@@ -124,6 +136,7 @@ const labFunction = () =>  {
         counter++;
     }
 
+    // заполнение таблицы в HTML
     iterationMethodTds[2].innerHTML = `${c}`;
     iterationMethodTds[3].innerHTML = `${counter}`;
 }
